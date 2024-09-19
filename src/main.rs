15e98630b9122bcaaaa0cb7ade04f6dc81419385
yanquankun <yanquankun@58.com>
2024-09-19 14:25:36 +0800

    add: 函数

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
  * 2024-09-18 18:39:03
  * @author Mint.Yan
  * @description 学习 Rust Good Good Study Day Day Up
- * @link https://kaisery.github.io/trpl-zh-cn/ch03-03-how-functions-work.html 
+ * @link https://kaisery.github.io/trpl-zh-cn/ch03-03-how-functions-work.html
 */
 
 // 2.猜数字游戏
@@ -123,5 +123,37 @@
 //     println!("{a:?} {a1}");
 // }
 
-// 3.3函数
-fn main() {}
+// 3.3 函数
+// fn main() {
+//     // 语句（Statements）是执行一些操作但不返回值的指令
+//     // 表达式（Expressions）计算并产生一个值
+
+//     // 这种写法是错误的，rust和其他语言不同，let y = 5 并不会产生返回值
+//     // let x = (let y = 5); ❌
+
+//     let y = {
+//         let x = 3;
+//         // 表达式的结尾没有分号。如果在表达式的结尾加上分号，它就变成了语句，而语句不会返回值
+//         x + 1
+//     };
+//     println!("The value of y is: {y}");
+
+//     // 所有函数名采用snake_case风格
+//     // 如果非要驼峰也可以运行，但不符合规范
+//     another_function(10, 666);
+
+//     let five = five();
+//     println!("The value of five is: {five}");
+// }
+
+// fn five() -> i32 {
+//     // 采用return或者无分号结尾都可以形成一个表达式
+//     // return 5;
+//     // or
+//     5
+// }
+
+// fn another_function(x: i8, y: i32) {
+//     println!("The value of x is: {x}");
+//     println!("The value of y is: {y}");
+// }
